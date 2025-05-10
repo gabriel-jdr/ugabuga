2b91d98fdeac090a4d44b80f9c0eda16bef53faf
gabriel-jdr <gabrieljesusderesende@gmail.com>
2025-05-09 21:25:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Permite todas as origens
   res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE'); // Permite os métodos especificados
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept" // Permite os cabeçalhos especificados
   );
@@ -22,26 +22,10 @@ const PORT = process.env.PORT || 3000;
 const routes = require('./routes/routes');
 app.use('/api', routes); // Prefixando as rotas com '/api'
 
-// Iniciando o servidor
+// Iniciando o servidor (funcionando para Render)
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
 });
 
-// Obtendo os parâmetros passados pela linha de comando
-var userArgs = process.argv.slice(2);
-var mongoURL = userArgs[0]; // URL do MongoDB passada como argumento
-
-// Configurando a conexão com o Banco de Dados MongoDB
-var mongoose = require('mongoose');
-mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }); // Adicionando algumas opções recomendadas
-mongoose.Promise = global.Promise;
-
-// Monitorando a conexão com o banco
-const db = mongoose.connection;
-
-// Se houver erro na conexão
-db.on('error', (error) => {
-  console.log('Erro de conexão:', error); // Mensagem de erro mais explicativa
-});
-
-// Quando a conexão for bem-sucedida
+// Obtendo a URL do MongoDB da variável de ambiente para maior segurança
+var mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/seu_
